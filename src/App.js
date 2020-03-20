19a7d52df95cb0de87747570737fba4b5eb76076
Chua Pei Kun <peikun@primetechnologies.com.sg>
2020-03-20 18:33:12 +0800

    Add stripe integration for checkout and update title and favicon of application

--- src/App.js
+++ src/App.js
@@ -50,8 +50,8 @@ class App extends React.Component {
         <Header/>
         <Switch>
           <Route exact path='/' component={ HomePage }/>
-          <Route exact path='/shop' component={ ShopPage }/>
-          <Route exect path='/checkout' component={ CheckoutPage } />
+          <Route path='/shop' component={ ShopPage }/>
+          <Route exact path='/checkout' component={ CheckoutPage } />
           <Route
             exact
             path='/signin'
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.components'
+import CheckoutItem from '../../components/checkout-item/checkout-item.components';
+import StripeCheckout from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 
@@ -35,6 +36,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>TOTAL: ${ total }</span>
     </div>
+    <StripeCheckout price={ total }/>
   </div>
 )
 
