cefde4fe277ae15bd1448c1da28e3820e8e6bcfc
Chua Pei Kun <peikun@primetechnologies.com.sg>
2020-03-25 21:42:03 +0800

    Use redux sage for google sign in

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
+import rootSaga from './root-saga'
 
-const middelwares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(...middelwares))
+const middlewares = [sagaMiddleware];
 
-const persistor = persistStore(store);
+if (process.env.NODE_ENV === 'development'){
+  middlewares.push(logger);
+}
 
-export { store, persistor };
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+sagaMiddleware.run(rootSaga)
+
+export const persistor = persistStore(store);
+
+export default { store, persistStore };
